fe04d56e95622de493ceaa1abbb12c17121a7049
Raksha P Rao <raksha.rao2009@gmail.com>
2019-11-19 10:47:06 +0000

    Added comments and description to the functions

--- src/solution_29c11459.py
+++ src/solution_29c11459.py
@@ -4,17 +4,32 @@ from ioOps import read_file, get_file_path, print_grid
 import json
 import sys
 
+"""
+Provides solution for the task 29c11459.json
+
+Args:
+    data(list): The data which needs to be processed
+
+Returns:
+    Solution for the input data
+"""
 def solve(data):
+    # The number associated with grey color
     grey = 5
     for input in data:
         for row in input:
             if row[0] != 0:
                 length = len(row)
+                # Finding mid cell of the row to color it grey
                 midPoint = length // 2
+                # getting the start color number
                 startCode = row[0]
+                # Getting the end color number
                 endCode = row[length - 1]
                 row[midPoint] = grey
+                # Filling start number from beginning of row to mid of row
                 row[1: midPoint] = startCode
+                # Filling end color number from mid to end of row
                 row[midPoint + 1: ] = endCode
     return data
 
--- src/solution_a699fb00.py
+++ src/solution_a699fb00.py
@@ -3,10 +3,21 @@ from ioOps import read_file, print_grid, get_file_path
 import json
 import sys
 
+"""
+Method to find solution for the task a699fb00.json
+
+Args:
+    data(list): The data which needs to be processed
+
+Returns:
+    Solution for the task
+"""
 def solve(data):
+    # For every input grid
     for input in data:
         for row in input:
             for index in range(len(row) - 2):
+                # Fill 2 for wvery cell which has cell numbered 1 to its left and right
                 if row[index] == 1 and row[index + 2] == 1:
                     row[index + 1] = 2
     return data
