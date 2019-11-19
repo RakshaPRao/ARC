cfd5166a00359f5fb19885defb6e48d0ce3559ff
suruchidgupta <suruchidgupta@gmail.com>
2019-11-19 12:04:10 +0000

    Adding solution files for 2 tasks

---
+++ src/solution_746b3537.py
@@ -0,0 +1,11 @@
+#This file has the solution to the task 746b3537.json
+
+def solve(data):
+
+    return data
+
+if __name__ == "__main__":
+    inputFilePath = get_file_path(sys.argv)
+    data = read_file(inputFilePath)
+    grid = solve(data)
+    print_grid(grid)
---
+++ src/solution_aabf363d.py
@@ -0,0 +1,11 @@
+# This file has the solution to the task aabf363d.json
+
+def solve(data):
+
+    return data
+
+if __name__ == "__main__":
+    inputFilePath = get_file_path(sys.argv)
+    data = read_file(inputFilePath)
+    grid = solve(data)
+    print_grid(grid)
