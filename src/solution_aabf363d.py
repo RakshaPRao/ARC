4a78d9cb5760283feb6c5fcee7485f124f34c656
suruchidgupta <suruchidgupta@gmail.com>
2019-11-21 10:57:15 +0000

    Adding solution for task aabf363d

--- src/solution_aabf363d.py
+++ src/solution_aabf363d.py
@@ -1,7 +1,14 @@
 # This file has the solution to the task aabf363d.json
+from ioOps import read_file, get_file_path, print_grid
+from itertools import groupby
+import numpy as np
+import sys
 
 def solve(data):
-
+    for input in data:
+        colour = input[-1][0]
+        input[-1][0] = 0
+        input[input != 0] = colour
     return data
 
 if __name__ == "__main__":
