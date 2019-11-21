9b10e839664aab1dfa70fb0406e34de22f3894f1
suruchidgupta <suruchidgupta@gmail.com>
2019-11-21 10:14:19 +0000

    Adding solution for task 746b3537

--- src/solution_746b3537.py
+++ src/solution_746b3537.py
@@ -1,8 +1,32 @@
 #This file has the solution to the task 746b3537.json
+from ioOps import read_file, get_file_path, print_grid
+from itertools import groupby
+import numpy as np
+import sys
+
+"""
+
+"""
 
 def solve(data):
-
-    return data
+    # For each input grid
+    output_list = []
+    for input in data:
+        input_list = input.tolist()
+        row = input_list[0]
+        column = [a[0] for a in input_list]
+        row = [k for k, g in groupby(row) if k != 0]
+        column = [k for k, g in groupby(column) if k != 0]
+        if len(row) == 1:
+            column_list = []
+            for i in column:
+                column_list.append([i])
+            print(column_list)
+            output_list.append(column_list)
+        else:
+            print(row)
+            output_list.append(row)
+    return output_list
 
 if __name__ == "__main__":
     inputFilePath = get_file_path(sys.argv)
